a1b442fc63ef6f2e2f9759a75330fb74270daaf2
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-03-18 16:08:40 +0600

    Switch to AndroidX bundled sqlite driver (#3082)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -218,7 +218,7 @@ dependencies {
     implementation(androidx.paging.runtime)
     implementation(androidx.paging.compose)
 
-    implementation(libs.bundles.sqlite)
+    implementation(androidx.sqlite.bundled)
     // SY -->
     implementation(sylibs.sqlcipher)
     // SY <--
