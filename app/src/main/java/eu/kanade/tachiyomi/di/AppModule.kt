132f31325a7a31ffa122dd093a1fa5c8c1adc672
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-03-23 20:13:39 +0600

    Potentially fix 'database is locked' crash
    # Conflicts:
    #	CHANGELOG.md
    #	app/src/main/java/eu/kanade/tachiyomi/di/AppModule.kt

--- app/src/main/java/eu/kanade/tachiyomi/di/AppModule.kt
+++ app/src/main/java/eu/kanade/tachiyomi/di/AppModule.kt
@@ -59,40 +59,49 @@ import uy.kohesive.injekt.api.addSingleton
 import uy.kohesive.injekt.api.addSingletonFactory
 import uy.kohesive.injekt.api.get
 import uy.kohesive.injekt.injectLazy
+import java.lang.ref.WeakReference
+
+private val lock = Any()
 
 class AppModule(val app: Application) : InjektModule {
     // SY -->
     private val securityPreferences: SecurityPreferences by injectLazy()
     // SY <--
 
+    private var sqlDriverRef: WeakReference<SqlDriver>? = null
+
     override fun InjektRegistrar.registerInjectables() {
         addSingleton(app)
 
         addSingletonFactory<SqlDriver> {
-            // SY -->
-            if (securityPreferences.encryptDatabase.get()) {
-                System.loadLibrary("sqlcipher")
+            synchronized(lock) {
+                sqlDriverRef?.get()?.let { return@synchronized it }
 
-                return@addSingletonFactory AndroidSqliteDriver(
-                    schema = Database.Schema,
-                    context = app,
-                    name = CbzCrypto.DATABASE_NAME,
-                    factory = SupportOpenHelperFactory(CbzCrypto.getDecryptedPasswordSql(), null, false, 25),
-                    callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
-                        override fun onOpen(db: SupportSQLiteDatabase) {
-                            super.onOpen(db)
-                            setPragma(db, "foreign_keys = ON")
-                            setPragma(db, "journal_mode = WAL")
-                            setPragma(db, "synchronous = NORMAL")
-                        }
+                // SY -->
+                if (securityPreferences.encryptDatabase.get()) {
+                    System.loadLibrary("sqlcipher")
 
-                        private fun setPragma(db: SupportSQLiteDatabase, pragma: String) {
-                            val cursor = db.query("PRAGMA $pragma")
-                            cursor.moveToFirst()
-                            cursor.close()
-                        }
-                    },
-                )
+                    return@synchronized AndroidSqliteDriver(
+                        schema = Database.Schema,
+                        context = app,
+                        name = CbzCrypto.DATABASE_NAME,
+                        factory = SupportOpenHelperFactory(CbzCrypto.getDecryptedPasswordSql(), null, false, 25),
+                        callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
+                            override fun onOpen(db: SupportSQLiteDatabase) {
+                                super.onOpen(db)
+                                setPragma(db, "foreign_keys = ON")
+                                setPragma(db, "journal_mode = WAL")
+                                setPragma(db, "synchronous = NORMAL")
+                            }
+
+                            private fun setPragma(db: SupportSQLiteDatabase, pragma: String) {
+                                val cursor = db.query("PRAGMA $pragma")
+                                cursor.moveToFirst()
+                                cursor.close()
+                            }
+                        },
+                    ).also { sqlDriverRef = WeakReference(it) }
+                }
             }
             // SY <--
 
@@ -103,7 +112,7 @@ class AppModule(val app: Application) : InjektModule {
                 configuration = AndroidxSqliteConfiguration(
                     isForeignKeyConstraintsEnabled = true,
                 ),
-            )
+            ).also { sqlDriverRef = WeakReference(it) }
         }
         addSingletonFactory {
             Database(
