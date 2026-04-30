5041a8655f15264aa1c4d621833dc7347f4b6b37
rianza <rianyanuar53@gmail.com>
2026-04-30 08:55:25 +0700

    build rinza

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "app.komikku"
+        applicationId = "app.moon"
 
         versionCode = 79
         versionName = "1.13.6"
