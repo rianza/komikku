0b7ca9f590498820d54b4e53f6c023176be11bbb
rianza <rianyanuar53@gmail.com>
2026-05-27 10:57:20 +0700

    fix: reinitialize logFilePrinter on app resume after release

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -304,6 +304,10 @@ class App : Application(), DefaultLifecycleObserver, SingletonImageLoader.Factor
     override fun onStart(owner: LifecycleOwner) {
         SecureActivityDelegate.onApplicationStart()
 
+        if (logFilePrinter == null) {
+            setupExhLogging()
+        }
+
         val syncPreferences: SyncPreferences = Injekt.get()
         val syncTriggerOpt = syncPreferences.getSyncTriggerOptions()
         if (syncPreferences.isSyncEnabled() && syncTriggerOpt.syncOnAppResume) {
@@ -318,6 +322,7 @@ class App : Application(), DefaultLifecycleObserver, SingletonImageLoader.Factor
     override fun onStop(owner: LifecycleOwner) {
         SecureActivityDelegate.onApplicationStopped()
         logFilePrinter?.releaseFileHandle()
+        logFilePrinter = null
 
         // AM (DISCORD) -->
         DiscordRPCService.stop(applicationContext)
