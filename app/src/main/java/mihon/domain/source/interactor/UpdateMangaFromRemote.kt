8c3d5d991d90891aff1ba78dedbb09dce74386d9
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-06-16 20:00:59 +0600

    Ensure chapter order is retained when passed to `source.getMangaUpdate`

--- app/src/main/java/mihon/domain/source/interactor/UpdateMangaFromRemote.kt
+++ app/src/main/java/mihon/domain/source/interactor/UpdateMangaFromRemote.kt
@@ -66,6 +66,7 @@ class UpdateMangaFromRemote(
     ): Result<RemoteMangaUpdate> {
         return try {
             val chapters = chapterRepository.getChapterByMangaId(manga.id)
+                .sortedBy { it.sourceOrder }
             val update = withIOContext {
                 // SY -->
                 if (source is EHentai) {
