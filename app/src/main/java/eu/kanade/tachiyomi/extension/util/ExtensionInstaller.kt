21cdfb4bd885ed9c7af1f509bc112f0eff161998
Jobobby04 <jobobby04@users.noreply.github.com>
2026-06-22 16:02:58 -0400

    Fix build

--- app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionInstaller.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionInstaller.kt
@@ -19,6 +19,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
 import logcat.LogPriority
 import okhttp3.OkHttpClient
 import okhttp3.Request
