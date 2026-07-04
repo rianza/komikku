ef44e1605fe2e0c9498600311fe44bc916aeb644
rianza <116883839+rianza@users.noreply.github.com>
2026-07-04 10:33:54 +0700

    clean'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,15 +23,6 @@ dependencyResolutionManagement {
         create("mihonx") {
             from(files("gradle/mihon.versions.toml"))
         }
-        create("kotlinx") {
-            from(files("gradle/kotlinx.versions.toml"))
-        }
-        create("androidx") {
-            from(files("gradle/androidx.versions.toml"))
-        }
-        create("compose") {
-            from(files("gradle/compose.versions.toml"))
-        }
     }
 
     @Suppress("UnstableApiUsage")
