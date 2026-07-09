c2905eb9dc77b9254495f98b4f5e56f8ef615083
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-07-09 14:11:56 +0600

    Add vertical chapter navigator for paged reader with height adjustment option (#3531)

--- app/src/main/java/eu/kanade/presentation/reader/components/ChapterNavigator.kt
+++ app/src/main/java/eu/kanade/presentation/reader/components/ChapterNavigator.kt
@@ -73,6 +73,7 @@ fun ChapterNavigator(
     // SY <--
     totalPages: Int,
     onPageIndexChange: (Int) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     val haptic = LocalHapticFeedback.current
 
@@ -111,7 +112,7 @@ fun ChapterNavigator(
     val textColor = MaterialTheme.colorScheme.onSurface
 
     if (type.isHorizontal()) {
-        ChapterNavigator(
+        HorizontalChapterNavigator(
             isRtl = type == ChapterNavigatorType.HORIZONTAL_RTL,
             state = state,
             onNextChapter = onNextChapter,
@@ -127,6 +128,7 @@ fun ChapterNavigator(
             backgroundColor = backgroundColor,
             buttonColor = buttonColor,
             textColor = textColor,
+            modifier = modifier,
         )
     } else {
         VerticalChapterNavigator(
@@ -144,12 +146,13 @@ fun ChapterNavigator(
             backgroundColor = backgroundColor,
             buttonColor = buttonColor,
             textColor = textColor,
+            modifier = modifier,
         )
     }
 }
 
 @Composable
-fun ChapterNavigator(
+fun HorizontalChapterNavigator(
     isRtl: Boolean,
     state: SliderState,
     onNextChapter: () -> Unit,
@@ -165,13 +168,14 @@ fun ChapterNavigator(
     backgroundColor: Color,
     buttonColor: IconButtonColors,
     textColor: Color,
+    modifier: Modifier = Modifier,
 ) {
     val layoutDirection = if (isRtl) LayoutDirection.Rtl else LayoutDirection.Ltr
 
     // We explicitly handle direction based on the reader viewer rather than the system direction
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
         Row(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .padding(horizontal = mainAxisPadding),
             verticalAlignment = Alignment.CenterVertically,
@@ -264,9 +268,10 @@ fun VerticalChapterNavigator(
     backgroundColor: Color,
     buttonColor: IconButtonColors,
     textColor: Color,
+    modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxHeight()
             .padding(vertical = mainAxisPadding),
         horizontalAlignment = Alignment.CenterHorizontally,
